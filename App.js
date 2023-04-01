d1ac5f6fcc2b7c5c91b52450d22778a7e380d32f
kalpesh chouhan <80672429+kalpeshchouhan@users.noreply.github.com>
2023-04-01 16:33:42 +0530

    changes

--- App.js
+++ App.js
@@ -28,7 +28,6 @@ mongoose.connect(
 app.use(bodyparser.json());
 app.use(express.urlencoded({ extends: false }));
 app.use(cors());
-app.use(cors({ origin: "https://kalpeshcurdapp.vercel.app" }));
 app.use("/student", rout);
 
 app.listen(port, () => {
