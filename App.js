5675a409b1e2a010923b3ec0ead96726dd4ecc0c
kalpesh chouhan <80672429+kalpeshchouhan@users.noreply.github.com>
2023-04-04 07:03:50 +0530

    changes

--- App.js
+++ App.js
@@ -23,11 +23,12 @@ mongoose.connect(
     log(e);
   }
 );
-app.use(
-  cors({
-    origin: "https://kalpeshcurdapp.vercel.app",
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 // middleware
 app.use(bodyparser.json());
 app.use(express.urlencoded({ extends: false }));
